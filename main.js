b9acfcf694a5bb6985f8c5040545cb0c92c49a8f
Jon Hazan <jon@hazan.me>
2014-06-14 19:42:01 +0400

    Setup the GET resources/:id endpoint

--- main.js
+++ main.js
@@ -3,16 +3,8 @@ var application_root = __dirname,
     path = require("path");//,
     //mongoose = require('mongoose');
 
-function sendFile(filename){
-	var outStream = require('fs').createWriteStream(filename);
-
-	// Add this to ensure that the out.txt's file descriptor is closed in case of error.
-	response.on('error', function(err) {
-		outStream.end();
-	});
-
-	// Pipe the input to the output, which writes the file.
-	response.pipe(outStream);
+function isInt(n){
+	return /^-?[0-9]+$/.test(n.toString());
 }
 
 function listResource(req, res) {
@@ -25,7 +17,58 @@ console.log(req);
 }
 
 function getResource(req, res) {
-  res.send('resource');
+	
+	var outtype = 'css';
+	switch(req.params.type){
+		case 'sass':
+			outtype = 'sass';
+		break;
+		case 'scss':
+			outtype = 'scss';
+		break;
+		case 'css':
+			outtype = 'css';
+		break;
+	}
+	
+	if(isInt(req.params.id)){
+		//@TODO: check file exists
+		var id = req.params.id
+		console.log('resources/'+req.params.id+'.'+outtype);
+	}else{
+		//@TODO: Better Error
+		res.send('Not a valid ID');
+		return
+	}
+	
+	var format = req.params.format || 'json'; //@TODO: Implement header to request format, Also set to json default
+	
+	switch(format){
+		case 'raw':
+			res.contentType('text/css');
+			res.sendfile('resources/'+id+'.'+outtype); //@TODO: filename
+		break;
+		case 'download':
+			format = 'download';
+			res.contentType('text/css');
+			res.download('resources/'+id+'.'+outtype, 'yourfile'.outtype); //@TODO: filename
+		break;
+		default:
+		case 'json':
+		
+			var links = {};
+			
+			settings.conversions.forEach(function(conv) {
+				links[conv] = {};
+				links[conv].links = {};
+				links[conv].links.raw = settings.url+'/resources/'+id+'/'+conv+'/raw';
+				links[conv].links.download = settings.url+'/resources/'+id+'/'+conv+'/download';
+				links[conv].status = 'Ready|Queued|Processing';
+				
+			});
+			res.send(links);
+		break;
+	}
 }
 
 function postreq(req, res) {
@@ -40,23 +83,21 @@ var express    = require('express'); 		// call express
 var app        = express(); 				// define our app using express
 var bodyParser = require('body-parser');
 
-// Database
-
-//mongoose.connect('mongodb://localhost/ecomm_database');
-
-// Config
+var settings = {
+	url: "http://assaas.hazan.me",
+	listen: 8080,
+	conversions: ['css','scss','sass']
+};
 
 var app = express();
 app.use(bodyParser());
 var router = express.Router();
 app.use('/', router);
-//server.use(express.static(path.join(application_root, "htdocs")));
-//server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 router.get('/', root);
 //server.get('/resources', listResource);
-router.get('/resources', createResource); //Should be POST but GET for testing
-router.get('/resources/:id', getResource);
+router.get('/resources', createResource); //@TODO: Should be POST but GET for testing
+router.get('/resources/:id/:type?/:format?', getResource);
 //router.post('/resources/:id', postreq);
 
 // Launch server

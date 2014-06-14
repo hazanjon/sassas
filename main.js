72d5c1ea108ab9492abd99c7fd30cd52f418e277
Jon Hazan <jon@hazan.me>
2014-06-14 18:43:48 +0400

    Switch to ExpressJS

--- main.js
+++ main.js
@@ -1,39 +1,64 @@
-var restify = require('restify');
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path");//,
+    //mongoose = require('mongoose');
 
-function listResource(req, res, next) {
+function sendFile(filename){
+	var outStream = require('fs').createWriteStream(filename);
+
+	// Add this to ensure that the out.txt's file descriptor is closed in case of error.
+	response.on('error', function(err) {
+		outStream.end();
+	});
+
+	// Pipe the input to the output, which writes the file.
+	response.pipe(outStream);
+}
+
+function listResource(req, res) {
   res.send('list of resources here');
-  next();
 }
 
-function createResource(req, res, next) {
-  res.send(req.params.css);
-  next();
+function createResource(req, res) {
+console.log(req);
+  res.send(req.query.css);
 }
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.css);
-  next();
+function getResource(req, res) {
+  res.send('resource');
 }
 
-function postreq(req, res, next) {
+function postreq(req, res) {
   res.send('hello ' + req.params.id);
-  next();
 }
 
-function root(req, res, next) {
+function root(req, res) {
   res.send('hi');
-  next();
 }
 
-var server = restify.createServer();
-server.use(restify.queryParser()).use(restify.bodyParser());
+var express    = require('express'); 		// call express
+var app        = express(); 				// define our app using express
+var bodyParser = require('body-parser');
 
-server.get('/', root);
+// Database
+
+//mongoose.connect('mongodb://localhost/ecomm_database');
+
+// Config
+
+var app = express();
+app.use(bodyParser());
+var router = express.Router();
+app.use('/', router);
+//server.use(express.static(path.join(application_root, "htdocs")));
+//server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+router.get('/', root);
 //server.get('/resources', listResource);
-server.get('/resources', createResource); //Should be POST but GET for testing
-server.get('/resources/:id', respond);
-server.post('/resources/:id', postreq);
+router.get('/resources', createResource); //Should be POST but GET for testing
+router.get('/resources/:id', getResource);
+//router.post('/resources/:id', postreq);
 
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
+// Launch server
+
+app.listen(8080);

411161ba36bae04328ecbe2636728257112e2764
Jon Hazan <jon@hazan.me>
2014-06-15 07:16:49 +0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -6,3 +6,10 @@ CREATE TABLE `users` (
   `apikey` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `resources` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
